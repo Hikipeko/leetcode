3114f3db82a5e290d03b7dae4f67ff82387f9b49
Hikipeko <1979160133@qq.com>
2021-11-11 15:58:02 -0500

    21

---
+++ solutions/0021_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,32 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *result = new ListNode;
+        ListNode *curr = result;
+        while (l1 && l2) {
+            if (l1->val > l2->val) {
+                curr->next = l2;
+                l2 = l2->next;
+            } else {
+                curr->next = l1;
+                l1 = l1->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+        curr = result->next;
+        delete result;
+        return curr;
+    }
+};
