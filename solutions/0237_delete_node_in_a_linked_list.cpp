fccd8603f2b64a16b60b8240fb016e75a9a3151e
Hikipeko <1979160133@qq.com>
2021-11-12 00:42:57 -0500

    237

---
+++ solutions/0237_delete_node_in_a_linked_list.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* next = node->next;
+        node->val = next->val;
+        node->next = next->next;
+        delete next;
+    }
+};
