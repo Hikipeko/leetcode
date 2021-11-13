d8e2061fd4bcda5d169bdc5b5fb8c8991482741d
Hikipeko <1979160133@qq.com>
2021-11-13 14:13:43 -0500

    287

--- solutions/0167_two_sum_2_input_array_is_sorted.cpp
+++ solutions/0167_two_sum_2_input_array_is_sorted.cpp
@@ -1,4 +1,29 @@
-//
-// Created by 19791 on 11/13/2021.
-//
+#include <unordered_map>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (true) {
+            if (nums[left] + nums[right] > target)
+                right --;
+            else if (nums[left] + nums[right] < target)
+                left++;
+            else break;
+        }
+        return vector<int> {left + 1, right + 1};
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2,7,11,15};
+    vector<int> t =  sol.twoSum(nums, 9) ;
+    for (auto it : t) cout << it << endl;
+    return 0;
+}
---
+++ solutions/0287_find_the_duplicate_number.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0, t = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) break;
+        }
+        while (true) {
+            slow = nums[slow];
+            t = nums[t];
+            if (slow == t) break;
+        }
+        return slow;
+    }
+};
