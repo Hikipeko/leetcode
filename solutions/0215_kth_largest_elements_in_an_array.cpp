bee64ed3fa8289e7607105e1d448844cb577db8d
Hikipeko <1979160133@qq.com>
2021-11-14 13:22:11 -0500

    215

---
+++ solutions/0215_kth_largest_elements_in_an_array.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int> &nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int n : nums) {
+            pq.push(n);
+            if (pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
