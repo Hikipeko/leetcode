c86b0608010a8288146a9bec2fd85f5039b1d3aa
Hikipeko <1979160133@qq.com>
2021-11-29 15:19:30 -0500

    73

---
+++ solutions/0073_set_matrix_zeroes.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>> &matrix) {
+        bool row0 = false, col0 = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0) row0 = true;
+                    if (j == 0) col0 = true;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.size(); i++)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < matrix[0].size(); j++)
+                    matrix[i][j] = 0;
+        for (int j = 1; j < matrix[0].size(); j++)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < matrix.size(); i++)
+                    matrix[i][j] = 0;
+        if (row0)
+            for (int j = 0; j < matrix[0].size(); j++)
+                matrix[0][j] = 0;
+        if (col0)
+            for (int i = 0; i < matrix.size(); i++)
+                matrix[i][0] = 0;
+    }
+};
