cea03778a49daff4325f7d446e45a7816999ba52
Hikipeko <1979160133@qq.com>
2021-12-03 18:25:10 -0500

    98

---
+++ solutions/0098_validate_binary_search_tree.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void inorderTraversal(TreeNode *root, vector<int> &v) {
+        if (!root) return;
+        if (root->left) inorderTraversal(root->left, v);
+        v.push_back(root->val);
+        if (root->right) inorderTraversal(root->right, v);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        vector<int> v;
+        inorderTraversal(root, v);
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i - 1] >= v[i]) return false;
+        }
+        return true;
+    }
+};
