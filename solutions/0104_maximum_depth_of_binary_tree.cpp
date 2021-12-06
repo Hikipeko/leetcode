84e98cca5a4a3f95266c51dcf0baef1f04e87774
DESKTOP-BT07A69\19791 <1979160133@qq.com>
2021-12-05 23:26:24 -0500

    104

---
+++ solutions/0104_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,26 @@
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int max(int l, int r) {return l > r ? l : r;}
+
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
