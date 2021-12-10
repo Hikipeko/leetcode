6cb5bd1cb486dcc23b52bbe7ada9bbbcdf2a95d1
DESKTOP-BT07A69\19791 <1979160133@qq.com>
2021-12-10 02:55:00 -0500

    236

--- solutions/0230_kth_smallest_element_in_a_bst.cpp
+++ solutions/0230_kth_smallest_element_in_a_bst.cpp
@@ -1,4 +1,34 @@
-//
-// Created by 19791 on 2021/12/9.
-//
+#include <vector>
+using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<TreeNode*> s;
+        TreeNode* curr = root;
+        while (curr || !s.empty()) {
+            while (curr) {
+                s.push_back(curr);
+                curr = curr->left;
+            }
+            curr = s.back();
+            s.pop_back();
+            k--;
+            if (k==0) return curr->val;
+            curr = curr->right;
+        }
+        return -1;
+    }
+};
