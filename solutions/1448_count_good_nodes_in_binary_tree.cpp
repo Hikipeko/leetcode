62dcd8b7557103e4fb53999d68c34e76a739b828
DESKTOP-BT07A69\19791 <1979160133@qq.com>
2021-12-12 03:31:25 -0500

    1448

--- solutions/1448_count_good_nodes_in_binary_tree.cpp
+++ solutions/1448_count_good_nodes_in_binary_tree.cpp
@@ -1,4 +1,33 @@
-//
-// Created by 19791 on 2021/12/12.
-//
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        int res = 0;
+        help(root, res, -114514);
+        return res;
+    }
+
+    void help(TreeNode* root, int &res, int max) {
+        if (!root) return;
+        if (root->val >= max) {
+            res++;
+            help(root->left, res, root->val);
+            help(root->right, res, root->val);
+        } else {
+            help(root->left, res, max);
+            help(root->right, res, max);
+        }
+
+    }
+};
