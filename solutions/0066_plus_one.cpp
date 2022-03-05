d8f809d7b5dded0052b1fc72c1b982df213d33ff
Hikipeko <joelchen@umich.edu>
2022-03-04 19:50:46 -0500

    66

--- solutions/0066_plus_one.cpp
+++ solutions/0066_plus_one.cpp
@@ -1,7 +1,20 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size() - 1;
-        
+        digits[n]++;
+        while (n > 0 && digits[n] == 10) {
+            digits[n] = 0;
+            digits[--n]++;
+        }
+        if (digits[0] == 10) {
+            digits[0] = 0;
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
     }
 };
