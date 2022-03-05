fe5c54038dbfe2ba4c7e345e0a029169527cec5c
Hikipeko <joelchen@umich.edu>
2022-03-05 17:59:18 -0500

    34

--- solutions/0034_find_first_and_last_position_of_elemnet_in_sorted_array.cpp
+++ solutions/0034_find_first_and_last_position_of_elemnet_in_sorted_array.cpp
@@ -1,11 +1,30 @@
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> temp(2,-1);
+        if (nums.empty()) return temp;
+        int l = 0, n = nums.size(), r = n, m;
+        vector<int> res;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (nums[m] >= target) r = m;
+            else l = m + 1;
+        }
+        if (l >= n || nums[l] != target) return temp;
+        res.push_back(l);
 
+        l = 0, r = n;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (nums[m] <= target) l = m + 1;
+            else r = m;
+        }
+        l--;
+        res.push_back(l);
+        return res;
     }
 };
