66fbb3068f3ead230cbacc1acd1ef5b0c92ccbd3
Hikipeko <1979160133@qq.com>
2022-03-25 17:52:39 -0400

    54

--- solutions/0054_spiral_matrix.cpp
+++ solutions/0054_spiral_matrix.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -6,7 +5,21 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        res = vector<int>();
-
+        vector<int> res;
+        int d = matrix.size();
+        int r = matrix[0].size();
+        int l = 0, u = 0, x = 0, y = 0;
+        while (true) {
+            while (y < r - 1) res.push_back(matrix[x][y++]);
+            if (++u > d) break;
+            while (x < d - 1) res.push_back(matrix[x++][y]);
+            if (--r < l) break;
+            while (y > l) res.push_back(matrix[x][y--]);
+            if (--d < u) break;
+            while (x > u) res.push_back(matrix[x--][y]);
+            if (++l > r) break;
+        }
+        res.push_back(matrix[x][y]);
+        return res;
     }
 };
