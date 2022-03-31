76aa72d90e18cf353f5b808f352d40e2104b17be
Hikipeko <1979160133@qq.com>
2022-03-31 16:37:44 -0400

    138

--- solutions/0138_copy_list_with_random_pointer.cpp
+++ solutions/0138_copy_list_with_random_pointer.cpp
@@ -1,9 +1,12 @@
-// Definition for a Node.
+#include <unordered_map>
+
+using namespace std;
+
 class Node {
 public:
     int val;
-    Node* next;
-    Node* random;
+    Node *next;
+    Node *random;
 
     Node(int _val) {
         val = _val;
@@ -14,7 +17,26 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
-
+    Node *copyRandomList(Node *head) {
+        unordered_map<Node *, Node *> old2new;
+        old2new[nullptr] = nullptr;
+        if (head == nullptr) return nullptr;
+        Node *res = new Node(head->val);
+        Node *curr = res, *old = head;
+        while (old->next != nullptr) {
+            old2new[old] = curr;
+            old = old->next;
+            curr->next = new Node(old->val);
+            curr = curr->next;
+        }
+        old2new[old] = curr;
+        old = head;
+        curr = res;
+        while (old != nullptr) {
+            curr->random = old2new[old->random];
+            curr = curr->next;
+            old = old->next;
+        }
+        return res;
     }
 };
