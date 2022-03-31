092de8a4c930db3cca87405e5aa794ae5e5c4aeb
Hikipeko <joelchen@umich.edu>
2022-03-30 23:59:50 -0400

    134

--- solutions/0134_gas_station.cpp
+++ solutions/0134_gas_station.cpp
@@ -6,13 +6,16 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
-        int sum = 0, max = -1, index = 0;
+        int sum = 0, total = 0, res = -1;
         for (int i = 0; i < n; i++) {
             gas[i] -= cost[i];
+            total += gas[i];
             sum += gas[i];
-            if (gas[i] > max) {
-                max = 
+            if (sum < 0) {
+                res = i + 1;
+                sum = 0;
             }
         }
+        return total < 0 ? -1 : res;
     }
 };
