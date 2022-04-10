0293e3fcccff612505819dee9a1e862987feb23b
Hikipeko <joelchen@umich.edu>
2022-04-09 20:45:42 -0400

    191

--- solutions/0190_reverse_bits.cpp
+++ solutions/0190_reverse_bits.cpp
@@ -3,7 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
-            res = res << 1 + n & 1;
+            res = (res << 1) + (n & 1);
             n = n >> 1;
         }
         return res;
