4f6dbf7d8348e6d5ae33950a849ced1a1e5d653c
Hikipeko <1979160133@qq.com>
2022-05-04 19:20:56 -0400

    240

---
+++ solutions/0240_search_a_2D_matrixII.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int i = 0, j = n - 1;
+        while (i != m && j != -1) {
+            if (matrix[i][j] < target) i++;
+            else if (matrix[i][j] > target) j--;
+            else return true;
+        }
+        return false;
+    }
+};
